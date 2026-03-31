ac789f383ea811bb8349a299fadcfd9a4f1d4fee
Dhanunjaya235 <120103263+Dhanunjaya235@users.noreply.github.com>
2026-03-31 14:46:06 +0530

    Added S3 Bucket

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+    required_version = ">= 1.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+
+    backend "s3" {
+        bucket         = "cognine-terraform-states"
+        region         = "ap-south-1"
+        encrypt        = true
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+
+    default_tags {
+        tags = local.common_tags
+    }
+}
+
+locals {
+    common_tags = {
+        Environment = var.environment
+        ManagedBy   = "Terraform"
+        Project     = var.project_name
+    }
+}
+
+resource "aws_s3_bucket" "main" {
+    bucket = "github-actions-demo-${var.environment}"
+}
